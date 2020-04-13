6aba5d11ef6c0c9b9ba93775cf0ccd478f76f12d
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-04-13 13:22:45 +0600

    Angular Module

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {ProductListComponent} from './product-list.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {RouterModule} from '@angular/router';
+import {ProductDetailGuard} from './guards/product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        canActivate: [ProductDetailGuard]
+      }
+    ]),
+    SharedModule,
+  ]
+})
+export class ProductModule { }
